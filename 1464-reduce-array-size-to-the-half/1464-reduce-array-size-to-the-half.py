47130db0cf95559b99774477b30bfaf103231f7b
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2024-09-08 20:56:36 -0400

    Time: 430 ms (68.73%), Space: 33.8 MB (94.37%) - LeetHub

---
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        arrSizeGoal = round(len(arr) / 2)
+        count = 0
+
+        counts = Counter(arr)
+        ordered = sorted(counts.values(), reverse=True)
+
+        currArrSize= len(arr)
+        for frequency in ordered:
+            count += 1
+            if currArrSize - frequency <= arrSizeGoal:
+                return count
+            else:
+                currArrSize = currArrSize - frequency
+
+        
