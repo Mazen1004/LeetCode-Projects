65142f76b68f48158c66d598c4a270ab9aadc3a4
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2024-11-24 11:52:42 -0500

    Time: 1 ms (56.32%), Space: 21.9 MB (49.71%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,17 +3,15 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
-        left=0
-        right=len(s)-1
-        temp=""
-        
+        left = 0
+        right = len(s) - 1
+
         while left < right:
-            #if s[left] != s[right]: unneccessary as it causes problems with incrementing/decrementing
-            temp = s[right] #stores right in temp var
-            s[right] = s[left] #copies left in right
-            s[left] = temp #copies temp right in left
-            right-=1
-            left+=1
-        return s
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1
+
+
         
