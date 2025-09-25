297136528367c0d2b4e54635dfad25067e5e9573
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2025-09-25 09:28:33 -0400

    Time: 11 ms (72.53%), Space: 18 MB (34.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        scounts = {}
-        tcounts = {}
-
+        countS = {}
+        countT = {}
+        
         for letter in s:
-            if letter in scounts:
-                scounts[letter] += 1
+            if letter in countS:
+                countS[letter] += 1
             else:
-                scounts[letter] = 1
+                countS[letter] = 1
 
         for letter in t:
-            if letter in tcounts:
-                tcounts[letter] += 1
+            if letter in countT:
+                countT[letter] += 1
             else:
-                tcounts[letter] = 1
+                countT[letter] = 1
 
-        return scounts == tcounts
 
-        
+        return countS == countT
